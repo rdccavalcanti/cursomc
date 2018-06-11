134ffe6b298603cedb8dcfd1182d3e371082be5e
Ricardo <2030ricardo@gmail.com>
2018-06-11 15:42:11 -0300

    Ajustes no endpoint /categorias/ {id}

--- src/main/java/com/nelioalves/cursomc/domain/Produto.java
+++ src/main/java/com/nelioalves/cursomc/domain/Produto.java
@@ -12,6 +12,8 @@ import javax.persistence.JoinColumn;
 import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
 
+import com.fasterxml.jackson.annotation.JsonBackReference;
+
 
 @Entity
 public class Produto implements Serializable{
@@ -19,9 +21,11 @@ public class Produto implements Serializable{
 	@Id
 	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private Integer id;
+	
 	private String nome;
 	private Double preco;
 	
+	@JsonBackReference
 	@ManyToMany
 	@JoinTable(name = "PRODUTO_CATEGORIA",    //JoinTable é uma tabela intermediaria
 	joinColumns = @JoinColumn(name = "produto_id"),
